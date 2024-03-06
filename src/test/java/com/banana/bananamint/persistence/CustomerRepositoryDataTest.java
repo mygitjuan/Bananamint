305fcda9e623132eda393683215932cc273a1a73
myGitJuan <jarellano.aguirre@gmail.com>
2024-03-06 13:29:15 +0100

    creacion persistencia

---
+++ src/test/java/com/banana/bananamint/persistence/CustomerRepositoryDataTest.java
@@ -0,0 +1,59 @@
+package com.banana.bananamint.persistence;
+
+import com.banana.bananamint.domain.Customer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ComponentScan("com.banana.bananamint.persistence")
+@AutoConfigureTestEntityManager
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CustomerRepositoryDataTest {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerRepositoryDataTest.class);
+
+    @Autowired
+    private CustomerRepositoryData repo;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    void findAll() throws Exception {
+        // given SQL Inserts
+        Customer usu = new Customer(null,"FAKE","j@j.com", LocalDate.now(),"04653011L");
+        em.persist(usu);
+        //em.remove(usu); //quiero usar un Entity Manager, pero no quiero hacer el alta, solo quiero retornar datos
+        //em.flush();
+
+        // when
+
+        List<Customer> usuList = repo.findAll();
+        logger.info("Lista usuarios:" + usuList);
+
+        // then
+        assertThat(usuList.size())
+                .isGreaterThan(0);
+
+        assertNotNull(usuList);
+
+    }
+
+
+}
