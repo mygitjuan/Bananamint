556993a5f5042883e436f0fb282b0db00d730769
abelenl <anabelen.llorente@dxc.com>
2024-03-07 09:46:21 +0100

    modificar definición tablaS part2

--- src/test/java/com/banana/bananamint/controller/IncomeExpenseControllerTest.java
+++ src/test/java/com/banana/bananamint/controller/IncomeExpenseControllerTest.java
@@ -28,19 +28,17 @@ class IncomeExpenseControllerTest {
         Account cta = new Account(1L);
         Expense gasto = new Expense(null, usu, 10, LocalDate.now(),cta, "pendiente");
 
-    /*    usu = custRepo.saveAndFlush(usu);
-        cta = accoRepo.saveAndFlush(cta);*/
-
         System.out.println("usu:" + usu);
         System.out.println("cta:" + cta);
         System.out.println("gasto:" + gasto);
 
         ResponseEntity<Expense> response = controller.anadeGastosCuenta(usu.getId(),cta.getId(),gasto);
-/*
-        System.out.println("respuesta gasto:" + response.getBody());
 
-        assertThat(response.getStatusCode().value()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.getBody()).isNotNull();*/
+        System.out.println("respuesta gasto:" + response);
+        System.out.println("gasto:" + response.getBody());
+
+        //assertThat(response.getStatusCode().value()).isEqualTo(HttpStatus.OK.value());
+        //assertThat(response.getBody()).isNotNull();
     }
 
     @Test
